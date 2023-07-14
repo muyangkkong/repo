6edaa1eef32e36269b8e6ec1e5fe1fc3dd97407f
LapisDJ <deokjung2001@gmail.com>
2023-07-15 05:33:23 +0900

    fix: 로딩바 해결

--- Assets/Code/Scripts/Loading.cs
+++ Assets/Code/Scripts/Loading.cs
@@ -6,38 +6,55 @@ using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
 {
+    public Slider progressbar; // 로딩 바 슬라이더
+    public float delayTime = 1.5f; // 로딩 바가 100%로 채워진 후 대기할 시간
 
-    public Slider progressbar;
-    
+    private bool loadingComplete = false; // 로딩 완료 여부
+
     private void Start()
     {
-        StartCoroutine(LoadScene());
+        progressbar = GameObject.Find("Slider").GetComponent<Slider>(); // GameObject에서 Slider 컴포넌트를 찾아 할당
+        StartCoroutine(LoadScene()); // 코루틴으로 LoadScene 호출
     }
 
     IEnumerator LoadScene()
     {
         yield return null;
-        AsyncOperation operation = SceneManager.LoadSceneAsync("3dScene");
-        operation.allowSceneActivation = false;
 
-        while(!operation.isDone){
-            yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync("3dScene"); // 3D Scene 비동기 로드
+        operation.allowSceneActivation = false; // Scene 활성화 지연
 
-            if(progressbar.value<0.9f){
-                progressbar.value=Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
-            }else if(operation.progress>=0.9f)
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f); // 실제 진행도 계산
+
+            Debug.Log("Loading progress: " + progress);
+            Debug.Log("Is loading complete? " + operation.isDone);
+
+            if (progressbar.value < progress) // 로딩 진행도가 실제 진행도보다 작을 경우
             {
-                progressbar.value=Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+                progressbar.value = Mathf.MoveTowards(progressbar.value, progress, Time.deltaTime * 0.5f); // 로딩 바 증가
             }
 
-
-            if(progressbar.value>=1f&&operation.progress>=0.9f){
-                operation.allowSceneActivation = true;
+            if (progressbar.value >= 1f && progress >= 1f) // 로딩 바와 실제 진행도가 모두 100% 이상인 경우
+            {
+                loadingComplete = true; // 로딩 완료 상태로 변경
+                yield return new WaitForSeconds(delayTime); // 대기 시간 동안 대기
+                operation.allowSceneActivation = true; // Scene 활성화
             }
+
+            yield return null;
         }
     }
-
-
-   
 }
+
+
+
+
+
+
+
+
+
+
 
