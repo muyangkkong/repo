903b253429755184e466ba64f8bd78fbc3b6a5b6
muyangkkong <137609813+muyangkkong@users.noreply.github.com>
2023-07-15 20:01:44 +0900

    feat-new:미니맵 스크립트 추가

---
+++ Assets/Code/Scripts/MinimapPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPlayer : MonoBehaviour
+{
+    private GameObject minimapplayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plyer=GetComponent<minimapplayer>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
