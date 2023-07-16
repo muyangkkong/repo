9f54988527b53e2e7e1b8a49ea6d303726951540
lickelon <jindoman0@naver.com>
2023-07-17 03:03:34 +0900

    feat-new: AttackData 추가

---
+++ Assets/Code/Scripts/Instruments/AttackData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackData : MonoBehaviour
+{
+    public AnimationClip animationClip;
+    public void init() {
+
+    }
+    public virtual void Attack() {
+
+    }
+
+    public virtual void Attack(Vector3 position) {
+
+    }
+
+    public virtual void Attack(Vector3 position, int direction) {
+
+    }
+}
