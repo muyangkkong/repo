1bf91f7cf2c5b8ae37752e3b6fa30aab5745a6c2
LapisDJ <deokjung2001@gmail.com>
2023-07-23 21:31:31 +0900

    feat: 메뉴, 로딩 화면 ai파일 적용

--- Assets/Code/Scripts/System/Loading.cs
+++ Assets/Code/Scripts/System/Loading.cs
@@ -1,14 +1,14 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
 {
+    public Slider progressbar; // 로딩바 슬라이더
+    public Image loadingImage; // 로딩 진행 상황에 따라 나타날 이미지
+    public float revealSpeed = 1f; // 이미지가 나타날 속도
 
-    public Slider progressbar;
-    
     private void Start()
     {
         StartCoroutine(LoadScene());
@@ -20,24 +20,27 @@ public class Loading : MonoBehaviour
         AsyncOperation operation = SceneManager.LoadSceneAsync("3dScene");
         operation.allowSceneActivation = false;
 
-        while(!operation.isDone){
+        while (!operation.isDone)
+        {
             yield return null;
 
-            if(progressbar.value<0.9f){
-                progressbar.value=Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
-            }else if(operation.progress>=0.9f)
+            // 로딩 진행 상황에 따라 로딩바 조절
+            float targetProgress = operation.progress < 0.9f ? 0.9f : 1f;
+            progressbar.value = Mathf.MoveTowards(progressbar.value, targetProgress, Time.deltaTime);
+
+            // 로딩 진행 상황에 따라 이미지 표시
+            float alphaValue = progressbar.value - 0.9f;
+            Color imageColor = loadingImage.color;
+            imageColor.a = alphaValue;
+            loadingImage.color = imageColor;
+
+            // 로딩이 완료되고 이미지가 완전히 나타났을 때 씬 전환
+            if (progressbar.value >= 1f && operation.progress >= 0.9f)
             {
-                progressbar.value=Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
-            }
-
-
-            if(progressbar.value>=1f&&operation.progress>=0.9f){
                 operation.allowSceneActivation = true;
             }
+
+            yield return null;
         }
     }
-
-
-   
 }
-
