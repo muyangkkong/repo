0c2d6711b15615c45bc6f37c42771826ef2d203a
ma_wurong <wurong1214@gmail.com>
2023-07-25 01:41:44 +0900

    feat-new: Map1,2,3 만듦 feat-improve: 카메라 이동 개선

--- Assets/Code/Scripts/System/MainCamera.cs
+++ Assets/Code/Scripts/System/MainCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    /*
     public Transform target; // ���ΰ��� Transform ������Ʈ�� �����ϱ� ���� ����
 
     private void LateUpdate()
@@ -12,6 +13,28 @@ public class MainCamera : MonoBehaviour
         Vector3 newPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
         transform.position = newPosition;
     }
+    */
+    
+    public Transform target;
 
+    public float smoothSpeed = 3;
+    public Vector2 offset;
+    public float limitMinX, limitMaxX, limitMinY, limitMaxY;
+    float cameraHalfWidth, cameraHalfHeight;
 
+    private void Start()
+    {
+        cameraHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+        cameraHalfHeight = Camera.main.orthographicSize;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(
+            Mathf.Clamp(target.position.x + offset.x, limitMinX + cameraHalfWidth, limitMaxX - cameraHalfWidth),   // X
+            Mathf.Clamp(target.position.y + offset.y, limitMinY + cameraHalfHeight, limitMaxY - cameraHalfHeight), // Y
+            -10);                                                                                                  // Z
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+    }
+    
 }
