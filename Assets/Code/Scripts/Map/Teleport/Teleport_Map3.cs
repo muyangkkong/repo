6576f28b225c979ce7e9ad0eac1adba8c2b5eece
ma_wurong <wurong1214@gmail.com>
2023-07-27 06:13:38 +0900

    feat-fix: 멥 추가 및 수정

---
+++ Assets/Code/Scripts/Map/Teleport/Teleport_Map3.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_Map3 : MonoBehaviour
+{
+    GameObject maincam;
+    MainCamera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincam = GameObject.Find("Main Camera");
+        cam = maincam.GetComponent<MainCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          
+            MapManager.Instance.DestroyMap();
+            MapManager.Instance.LoadMap(3);
+            MapManager.Instance.BuildMap();
+            Minimap.Instance.LoadMap(3);
+            Minimap.Instance.FindingPlayer();
+            Minimap.Instance.UpdateMiniMap();
+            Minimap.Instance.Init();
+            cam.limitMaxX = 99.5f;
+            cam.limitMaxY = 100f;
+            //Debug.Log(map.gameObject.GetInstanceID());
+        }
+
+    }
+}
---
+++ Assets/Code/Scripts/Map/Teleport/Teleport_Map8.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_Map8 : MonoBehaviour
+{
+    GameObject maincam;
+    MainCamera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincam = GameObject.Find("Main Camera");
+        cam = maincam.GetComponent<MainCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          
+            MapManager.Instance.DestroyMap();
+            MapManager.Instance.LoadMap(8);
+            MapManager.Instance.BuildMap();
+            Minimap.Instance.LoadMap(8);
+            Minimap.Instance.FindingPlayer();
+            Minimap.Instance.UpdateMiniMap();
+            Minimap.Instance.Init();
+            cam.limitMaxX = 99.5f;
+            cam.limitMaxY = 100f;
+            //Debug.Log(map.gameObject.GetInstanceID());
+        }
+
+    }
+}
