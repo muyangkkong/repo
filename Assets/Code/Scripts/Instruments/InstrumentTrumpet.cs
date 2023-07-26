3810287fa15d6cfde0e864cf9fa109b9533c7dad
Odia4 <sugar00025@naver.com>
2023-07-27 00:56:13 +0900

    Fix:엑셀 파일 수정

--- Assets/Code/Scripts/Instruments/InstrumentTrumpet.cs
+++ Assets/Code/Scripts/Instruments/InstrumentTrumpet.cs
@@ -32,13 +32,7 @@ public class InstrumentTrumpet : Instrument
 
 
     // Start is called before the first frame update
-    public override void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    public override void Update()
+    public override void Init()
     {
         
     }
