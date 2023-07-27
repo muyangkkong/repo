515f8e4a4241829594a8e6bd33e2fcc11e2b37df
ma_wurong <wurong1214@gmail.com>
2023-07-28 04:07:00 +0900

    feat:new 로딩 화면 추가

--- Assets/Code/Scripts/Map/CanvasManager.cs
+++ Assets/Code/Scripts/Map/CanvasManager.cs
@@ -8,20 +8,29 @@ public class CanvasManager : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject[] uiObject; // 활성화 또는 비활성화할 UI 오브젝트
-
+    GameObject targetplayer;
+    PlayerMovement move;
+     void Awake()
+    {
+        targetplayer = GameObject.Find("Player");
+        move = targetplayer.GetComponent<PlayerMovement>();
+    }
     // UI를 활성화하는 함수
 
     IEnumerator ActivateAndDeactivateUI()
     {
        
-        yield return new WaitForSeconds(0.00000025f); //
+        yield return new WaitForSeconds(0.3f); //
         uiObject[1].SetActive(false);
         uiObject[0].SetActive(true);
-        yield return new WaitForSeconds(0.0000005f);
+        yield return new WaitForSeconds(0.4f);
         uiObject[0].SetActive(false);
         uiObject[1].SetActive(false);
         uiObject[2].SetActive(true);
         uiObject[3].SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        move.speed = 10.0f;
+
     }
 
     // 코루틴 실행을 시작하는 함수
@@ -36,6 +45,7 @@ public class CanvasManager : MonoBehaviour
         uiObject[1].SetActive(false);
         uiObject[2].SetActive(true);
         uiObject[3].SetActive(true);
+        
     }
     public void Activate()
     {
@@ -48,6 +58,7 @@ public class CanvasManager : MonoBehaviour
         uiObject[0].SetActive(false);
         uiObject[2].SetActive(false);
         uiObject[3].SetActive(false);
+        move.speed = 0f;
     }
     // Update is called once per frame
     void Update()
