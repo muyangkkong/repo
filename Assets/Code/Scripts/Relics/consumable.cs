83527130fd0ece18db997b7dc49fe1b6b367daa2
muyangkkong <137609813+muyangkkong@users.noreply.github.com>
2023-08-10 02:09:13 +0900

    fix:유물,악보 구현안되는거

---
+++ Assets/Code/Scripts/Relics/consumable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class consumable : MonoBehaviour
+{
+    public Relic effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
